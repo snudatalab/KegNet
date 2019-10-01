ff93909a1d4028a6bb575e5994eaec735d8f32ae
Jaemin Yoo <jaeminyoo@snu.ac.kr>
2019-10-01 10:22:00 +0900

    Update the main script

--- src/main.py
+++ src/main.py
@@ -10,29 +10,28 @@ Authors:
 This software may be used only for research evaluation purposes.
 For other purposes (e.g., commercial), please contact the authors.
 """
-import os
-
 from kegnet.classifier.train import main as train_student
 from kegnet.generator.train import main as train_generator
 
 
 def main():
+    n_generators = 5
     dataset = 'mnist'
-    classifier = '../pretrained/{}.pth.tar'.format(dataset)
-    path_out = '../out/{}'.format(dataset)
-    index = 0
+    path_teacher = f'../pretrained/{dataset}.pth.tar'
+    path_out = f'../out/{dataset}'
 
-    gen_path = '../out/mnist/generator-200.pth.tar'
-    if not os.path.exists(gen_path):
-        gen_path = train_generator(dataset, classifier, path_out, index)
+    generators = []
+    for i in range(n_generators):
+        path_gen = f'{path_out}/generator-{i}'
+        path_model = train_generator(dataset, path_teacher, path_gen, i)
+        generators.append(path_model)
 
     data_dist = 'kegnet'
     option = 1
-    generators = [gen_path]
-    train_student(dataset, data_dist, path_out, index,
-                  load=classifier,
-                  generators=generators,
-                  option=option)
+    seed = 0
+    path_cls = f'{path_out}/classifier-{seed}'
+    train_student(dataset, data_dist, path_cls, seed, path_teacher, generators,
+                  option)
 
 
 if __name__ == '__main__':
